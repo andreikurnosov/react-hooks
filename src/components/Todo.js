a3bd54cc96047340f111f5a06885712a85c82408
nixdie <nixdie@yandex.ru>
2019-03-07 01:24:04 +0700

    sending data to firebase

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const todo = (props) => {
-  // const [todoName, setTodoName] = useState('');
-  // const [todoList, setTodoList] = useState([]);
-  const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+  const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
   const inputChangeHandler = (event) => {
-    setTodoState({
-      userInput: event.target.value,
-      todoList: todoState.todoList
-    });
+    // setTodoState({
+    //   userInput: event.target.value,
+    //   todoList: todoState.todoList
+    // });
+    setTodoName(event.target.value);
   };
 
   const todoAddHandler = () => {
-    setTodoState({
-      userInput: todoState.userInput,
-      todoList: todoState.todoList.concat(todoState.userInput)
-    });
+    // setTodoState({
+    //   userInput: todoState.userInput,
+    //   todoList: todoState.todoList.concat(todoState.userInput)
+    // });
+    setTodoList(todoList.concat(todoName));
+    axios
+      .post('https://vue-exp.firebaseio.com/todos.json', {name: todoName})
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -25,11 +36,11 @@ const todo = (props) => {
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={todoState.userInput}
+        value={todoName}
       />
       <button onClick={todoAddHandler}>Add</button>
       <ul>
-        {todoState.todoList.map((todo) => (
+        {todoList.map((todo) => (
           <li key={Math.random()}>{todo}</li>
         ))}
       </ul>
